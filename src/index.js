f72891175cc2eb5bbf9b7b980dac792f453d6ba4
Isadora <vs.isadora@gmail.com>
2020-03-13 16:57:25 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ import {Provider} from 'react-redux';
 import store from './store';
 
 import Routes from '~/routes';
-import {REACT_APP_GOOGLE_API_KEY} from 'react-native-dotenv';
-
-console.log('API');
-console.log(REACT_APP_GOOGLE_API_KEY);
 
 const App = () => (
   <Provider store={store}>
