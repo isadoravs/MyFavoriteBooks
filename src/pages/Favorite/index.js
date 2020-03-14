a3900c59d09a43cc7e0b3f73ee520d5993db947d
Isadora Vasconcellos <vs.isadora@gmail.com>
2020-03-14 18:18:56 -0300

    cria telas home e favoritos

---
+++ src/pages/Favorite/index.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+export default function FavoriteScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Favorite</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
