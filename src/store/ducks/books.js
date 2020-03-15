db89eaada56179d55b57c57ce8fc6ade7c0e9650
Isadora Vasconcellos <vs.isadora@gmail.com>
2020-03-15 15:05:25 -0300

    ajusta layout itens da lista de livros

--- src/store/ducks/books.js
+++ src/store/ducks/books.js
@@ -1,3 +1,4 @@
+import {useSelector} from 'react-redux';
 // Action Types
 import {REACT_APP_GOOGLE_API_KEY} from 'react-native-dotenv';
 
@@ -5,14 +6,19 @@ export const Types = {
   GET_BOOKS: 'books/LOAD',
   GET_BOOKS_SUCCESS: 'books/LOAD_SUCCESS',
   GET_BOOKS_FAIL: 'books/LOAD_FAIL',
+  GET_PAGE: 'page/LOAD',
+  GET_PAGE_SUCCESS: 'page/LOAD_SUCCESS',
+  GET_PAGE_FAIL: 'page/LOAD_FAIL',
 };
 
 // Reducer
 
 const initialState = {
   loading: false,
+  loadingPage: false,
   books: [],
-  error: [],
+  error: undefined,
+  totalItems: 0,
 };
 
 export default function reducer(state = initialState, action) {
@@ -21,9 +27,27 @@ export default function reducer(state = initialState, action) {
     case Types.GET_BOOKS:
       return {...state, loading: true};
     case Types.GET_BOOKS_SUCCESS:
-      return {...state, loading: false, books: action.payload.data};
+      return {
+        ...state,
+        loading: false,
+        books: action.payload.data.items,
+        totalItems: action.payload.data.totalItems,
+      };
     case Types.GET_BOOKS_FAIL:
-      return {...state, loading: false, error: action.payload.error};
+      return {...state, loading: false, error: action.error.message};
+    case Types.GET_PAGE:
+      return {...state, loadingPage: true};
+    case Types.GET_PAGE_SUCCESS:
+      return {
+        ...state,
+        loadingPage: false,
+        books: action.payload.data.items
+          ? [...state.books, ...action.payload.data.items]
+          : [...state.books],
+        totalItems: action.payload.data.totalItems,
+      };
+    case Types.GET_PAGE_FAIL:
+      return {...state, loadingPage: false, error: action.error.message};
     default:
       return state;
   }
@@ -31,9 +55,21 @@ export default function reducer(state = initialState, action) {
 
 // Action Creators
 
-export function getBooks(value, index) {
+export function getBooks(value) {
   return {
     type: Types.GET_BOOKS,
+    payload: {
+      request: {
+        url: `/volumes?q=${value}&startIndex=0&key=${REACT_APP_GOOGLE_API_KEY}`,
+      },
+    },
+  };
+}
+
+export function getPage(value, index) {
+  console.log(`Pagina ${index}`);
+  return {
+    type: Types.GET_PAGE,
     payload: {
       request: {
         url: `/volumes?q=${value}&startIndex=${index}&key=${REACT_APP_GOOGLE_API_KEY}`,
